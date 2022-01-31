bc1f7cc9b0347d0d00fd0c61510fbf386da80177
Felipe <felipefranca824@gmail.com>
2022-01-31 15:13:34 -0300

    Widgets comuns criados

--- lib/app/modules/splash/splash_page.dart
+++ lib/app/modules/splash/splash_page.dart
@@ -1,3 +1,6 @@
+import 'package:app_delivery/app/core/ui/widgets/vakinha_appbar.dart';
+import 'package:app_delivery/app/core/ui/widgets/vakinha_button.dart';
+import 'package:app_delivery/app/core/ui/widgets/vakinha_textformfield.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -5,6 +8,20 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: VakinhaAppBar(),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: VakinhaTextFormField(label: "Text Form"),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          VakinhaButton(onPressed: () {}, label: 'Entrar'),
+        ],
+      ),
+    );
   }
 }
