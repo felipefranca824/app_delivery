d24d7f00470354034a0cb2a891ba3b270f00e264
Felipe <felipefranca824@gmail.com>
2022-02-02 14:42:42 -0300

    Implementado BottomNavigationBar e Navigator

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,3 +1,41 @@
+import 'package:app_delivery/app/core/services/auth_services.dart';
+import 'package:app_delivery/app/modules/menu/menu_bindings.dart';
+import 'package:app_delivery/app/modules/menu/menu_page.dart';
+import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  static const NAVIGATOR_KEY = 1;
+
+  final _tabIndex = 0.obs;
+
+  int get tabIndex => _tabIndex.value;
+
+  final _tabs = [
+    '/menu',
+    '/order/shpping_card',
+    '/exit',
+  ];
+
+  set tabIndex(int index) {
+    _tabIndex(index);
+    if (_tabs[index] == '/exit') {
+      Get.find<AuthServices>().logout();
+    } else {
+      Get.toNamed(_tabs[index], id: NAVIGATOR_KEY);
+    }
+  }
+
+  Route? onGeneratedRouter(RouteSettings settings) {
+    if (settings.name == '/menu') {
+      return GetPageRoute(
+        settings: settings,
+        page: () => const MenuPage(),
+        binding: MenuBindings(),
+        transition: Transition.fadeIn,
+      );
+    }
+    if (settings.name == '/order/shopping_card') {}
+  }
+}
--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_delivery/app/core/services/auth_services.dart';
+import 'package:app_delivery/app/core/ui/widgets/vakinha_appbar.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import './home_controller.dart';
@@ -9,16 +10,34 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomePage'),
+      appBar: VakinhaAppBar(
+        elevation: 0,
       ),
-      body: Center(
-          child: TextButton(
-        onPressed: () {
-          Get.find<AuthServices>().logout();
-        },
-        child: const Text('logout'),
-      )),
+      bottomNavigationBar: Obx(() {
+        return BottomNavigationBar(
+          onTap: (value) => controller.tabIndex = value,
+          currentIndex: controller.tabIndex,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              label: 'Produtos',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: 'Carrinho',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.exit_to_app),
+              label: 'Sair',
+            )
+          ],
+        );
+      }),
+      body: Navigator(
+        initialRoute: '/menu',
+        key: Get.nestedKey(HomeController.NAVIGATOR_KEY),
+        onGenerateRoute: controller.onGeneratedRouter,
+      ),
     );
   }
 }
